a7460b6076f64060e11169b7a338c8b690afae1b
Dennis J Daubney Jr <dennis.daubney@gmail.com>
2017-12-16 20:11:01 -0500

    added comments for program purpose

--- firstDuplicate/firstDuplicate/Program.cs
+++ firstDuplicate/firstDuplicate/Program.cs
@@ -12,7 +12,8 @@ namespace firstDuplicate
         {
             int firstDuplicate(int[] a)
             {
-
+                // Write a solution with O(n) time complexity and O(1) additional space complexity,
+                // since this is what you would be asked to do during a real interview.
             }
         }
     }
