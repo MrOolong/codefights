0a21a49046e702d85e27a2993f10a0f8bfa52604
Dennis J Daubney Jr <dennis.daubney@gmail.com>
2017-12-21 20:34:44 -0500

    solution for centuryFromYear

---
+++ CenturyFromYear/CenturyFromYear/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CenturyFromYear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int centuryFromYear(int year)
+            {
+                int century;
+                if (year % 100 == 0)
+                {
+                    century = year / 100;
+                }
+                else
+                {
+                    century = year / 100 + 1;
+                }
+                return century;
+            }
+
+        }
+    }
+}
