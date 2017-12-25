947e5f4ed1eee41ff1ed4c6c11098e1f249fd45b
Dennis J Daubney Jr <dennis.daubney@gmail.com>
2017-12-24 22:13:12 -0500

    Add Two Digits challenge - initial commit

---
+++ AddTwoDigits/AddTwoDigits/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddTwoDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // You are given a two-digit integer n. Return the sum of its digits.
+        }
+    }
+}
